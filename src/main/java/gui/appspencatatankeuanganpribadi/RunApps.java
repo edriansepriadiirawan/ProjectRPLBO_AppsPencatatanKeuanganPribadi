a73ab83a67e7de2202821accc8052d80820aca46
iyanz <143705624+edriansepriadiirawan@users.noreply.github.com>
2025-05-08 01:27:02 +0700

    UpdatePengingatTransaksi

--- src/main/java/gui/appspencatatankeuanganpribadi/RunApps.java
+++ src/main/java/gui/appspencatatankeuanganpribadi/RunApps.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class RunApps extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(RunApps.class.getResource("Dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(RunApps.class.getResource("View-dataTransaksi.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         scene.getStylesheets().add(getClass().getResource("dashboard.css").toExternalForm());
         stage.setTitle("Hello gesss welkom bek to my canel");
